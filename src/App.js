198eb759d5dd397ce08baad744aabcfcec6342be
Horacio López <hlopez@Horacios-MacBook-Pro.local>
2019-11-24 21:06:16 -0300

    Added readme in spanish and some coded

--- src/App.js
+++ src/App.js
@@ -1,26 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    window.Mercadopago.setPublishableKey('TEST-fe993d79-6212-4cab-be80-c6457ba004b0');
+  }
+
+  render() {
+    return (
+      <form action="" method="post" id="pay" name="pay">
+  <fieldset>
+    <ul>
+      <li>
+        <label htmlFor="email">
+          Email
+        </label>
+        <input
+          id="email"
+          name="email"
+          defaultValue="test_user_71425066@testuser.com"
+          type="email"
+          placeholder="your email"
+        />
+      </li>
+      <li>
+        <label htmlFor="cardNumber">
+          Credit card number:
+        </label>
+        <input
+          type="text"
+          id="cardNumber"
+          data-checkout="cardNumber"
+          placeholder="4509953566233704"
+          autoComplete="off"
+          maxLength={16}
+        />
+      </li>
+      <li>
+        <label htmlFor="securityCode">
+          Security code:
+        </label>
+        <input
+          type="text"
+          id="securityCode"
+          data-checkout="securityCode"
+          placeholder="123"
+          autoComplete="off"
+        />
+      </li>
+      <li>
+        <label htmlFor="cardExpirationMonth">
+          Expiration month:
+        </label>
+        <input
+          type="text"
+          id="cardExpirationMonth"
+          data-checkout="cardExpirationMonth"
+          placeholder="12"
+          autoComplete="off" />
+      </li>
+      <li>
+        <label htmlFor="cardExpirationYear">
+          Expiration year:
+        </label>
+        <input
+          type="text"
+          id="cardExpirationYear"
+          data-checkout="cardExpirationYear"
+          placeholder="2015"
+          autoComplete="off"
+        />
+      </li>
+      <li>
+        <label
+          htmlFor="cardholderName">
+          Card holder name:
+        </label>
+        <input
+          type="text"
+          id="cardholderName"
+          data-checkout="cardholderName"
+          placeholder="APRO"
+        />
+      </li>
+      <li>
+        <label htmlFor="docType">
+          Document type:
+        </label>
+        <select id="docType" data-checkout="docType"></select>
+      </li>
+      <li>
+        <label htmlFor="docNumber">Document number:</label>
+        <input
+          type="text"
+          id="docNumber"
+          data-checkout="docNumber"
+          placeholder="12345678"
+        />
+      </li>
+    </ul>
+    <input
+      type="hidden"
+      name="paymentMethodId"
+    />
+    <input type="submit" value="Pay!" />
+  </fieldset>
+</form>
+    )
+  };
 }
 
 export default App;
